3349d11f711bf623eda0f4ed84d943bc48879ec3
Maciej Krüger <mkg20001@gmail.com>
2018-06-23 10:57:47 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# really-require
+
+A module that checks for unused and missing dependencies
+
+## API
+
+`reallyRequire(modulePath, options)`:
+ - `modulePath`: String, full or relative path to module
+ - `options`: String, path to package.json. Object, values of package.json. Default, `path.join(modulePath, 'package.json')`
+ - `sourceGlob`: String[], globs to use for searching the source files. Default, `['*.js', 'src/**/*.js', 'lib/**/*.js']`
+ - `nodeModules`: String[], paths to search in for modules. Default, `[path.join(modulePath, 'node_modules')]`
+
+Returns:
+ - `result.missing`: Object[] Messages about missing dependencies
+ - `result.missing[].error`: Flag if the warning is fatal
+ - `result.missing[].message`: Message to display
+ - `result.missing[].dependency`: Dependency name
+ - `result.missing[].location`: Location of the error
+ - `result.missing[].location.file`: Filename
+ - `result.missing[].location.from.line`: Line at which the statement begins
+ - `result.missing[].location.from.column`: Column at which the statement begins
+ - `result.missing[].location.to.line`: Line at which the statement ends
+ - `result.missing[].location.to.column`: Column at which the statement ends
+ - `result.unused`: Object[] Messages about unused dependencies
+ - `result.unused[].error`: Flag if the warning is fatal
+ - `result.unused[].message`: Message to display
+ - `result.unused[].dependency`: Dependency name
+ - `result.errors`: Object[] Parsing errors that occured
+ - `result.errors[].file`: File which caused the error
+ - `result.errors[].error`: Error Object
---
+++ src/index.js
@@ -0,0 +1,186 @@
+'use strict'
+
+const acorn = require('acorn')
+const walk = require('acorn/dist/walk')
+const path = require('path')
+const fs = require('fs')
+const promisify = require('util').promisify
+
+const read = promisify(fs.readFile)
+const glob = require('glob')
+
+const NATIVE_MODULES = [
+  'assert', 'buffer', 'child_process', 'cluster', 'crypto', 'dgram', 'dns', 'domain', 'events',
+  'fs', 'http', 'https', 'net', 'os', 'path', 'punycode', 'querystring', 'readline', 'stream',
+  'string_decoder', 'timers', 'tls', 'tty', 'url', 'util', 'v8', 'vm', 'zlib'] // src https://www.w3schools.com/nodejs/ref_modules.asp
+
+function getRequire (str) { // parses str with acorn and gets require() calls
+  let req = []
+
+  walk.full(acorn.parse(str), node => {
+    if (node.type === 'CallExpression' && node.callee.name === 'require') {
+      let arg = node.arguments[0]
+      if (arg && arg.type === 'Literal') {
+        let raw = arg.value
+        let name
+        let isModule = false
+        let isFile = false
+        switch (true) {
+          case raw.startsWith('/') || raw.startsWith('.'):
+            isFile = true
+            break
+          case raw.startsWith('@'):
+            isModule = true
+            name = raw.split('/').slice(0, 2).join('/')
+            break
+          default:
+            isModule = true
+            name = raw.split('/').slice(0, 1).join('/')
+            break
+        }
+        req.push({
+          start: node.start,
+          end: node.end,
+          name,
+          path: raw,
+          isModule,
+          isFile
+        })
+      } // TODO: else warn user maybe?
+    }
+  })
+
+  return req
+}
+
+function checkModule (options, dir, module) {
+  let location
+  try {
+    location = require.resolve(module.path, { paths: options.nodeModules.concat([dir]) })
+  } catch (e) {
+    location = null
+  }
+
+  return {
+    installed: Boolean(location),
+    location,
+    isDep: options.deps.indexOf(module.name) !== -1,
+    isDevDep: options.devDeps.indexOf(module.name) !== -1,
+    isNative: NATIVE_MODULES.indexOf(module.name) !== -1
+  }
+}
+
+function createWarning ({content, offset, file, dep, check, error}, message) {
+  let {start, end} = dep
+
+  if (offset) {
+    start += offset
+    end += offset
+    content = ' '.repeat(offset - 1) + '\n' + content
+  }
+
+  let split = content.substr(0, start).split('\n')
+  let line = split.length
+  let column = split.pop().length + 1
+
+  split = content.substr(0, end).split('\n')
+  let lineTo = split.length
+  let columnTo = split.pop().length + 1
+
+  return {
+    message,
+    error: error || false,
+    location: {
+      file,
+      from: {
+        line,
+        column
+      },
+      to: {
+        line: lineTo,
+        column: columnTo
+      }
+    },
+    dependency: dep.name,
+  }
+}
+
+async function reallyRequire (modulePath, options, cb) {
+  modulePath = fs.realpathSync(modulePath)
+  if (!options) { options = {} }
+  if (!options.packageJSON) { options.packageJSON = path.join(modulePath, 'package.json') }
+  if (!options.sourceGlob) { options.sourceGlob = ['*.js', 'src/**/*.js', 'lib/**/*.js'] } // unsafe alternative: !(node_modules)/**/*.js
+  if (!Array.isArray(options.sourceGlob)) { options.sourceGlob = [options.sourceGlob] }
+  if (!options.nodeModules) { options.nodeModules = [ path.join(modulePath, 'node_modules') ] }
+
+  let pkg
+  try {
+    pkg = typeof options.packageJSON === 'object' ? options.packageJSON : JSON.parse(String(await read(options.packageJSON)))
+  } catch (e) {
+    throw new Error('Couldn\'t read ' + options.packageJSON + ': ' + e.toString())
+  }
+
+  options.deps = Object.keys(pkg.dependencies)
+  options.devDeps = Object.keys(pkg.devDependencies)
+
+  let unused = Object.keys(pkg.dependencies)
+
+  let files = []
+  for (let i1 = 0; i1 < options.sourceGlob.length; i1++) {
+    files = files.concat(glob.sync(modulePath + '/' + options.sourceGlob[i1]))
+  }
+
+  let result = {
+    missing: [],
+    unused: [],
+    errors: []
+  }
+
+  for (let i2 = 0; i2 < files.length; i2++) {
+    let file = files[i2]
+
+    try {
+      let content = String(await read(file))
+      let offset = 0
+      if (content.startsWith('#')) { // remove #!
+        let s = content.split('\n')
+        offset += s.shift().length + 1
+        content = s.join('\n')
+      }
+
+      const required = getRequire(content)
+
+      for (let i3 = 0; i3 < required.length; i3++) {
+        let dep = required[i3]
+        let check = checkModule(options, path.dirname(file), dep)
+
+        if (dep.isFile) {
+          if (!check.installed) {
+            result.missing.push(createWarning({content, offset, file, dep, check, error: true}, 'Required file "' + dep.path + '" is missing!'))
+          }
+        } else if (dep.isModule && !check.isNative) {
+          unused = unused.filter(d => d !== dep.name)
+          if (check.isDevDep && !check.isDep) {
+            result.missing.push(createWarning({content, offset, file, dep, check}, 'Dependency "' + dep.name + '" is installed as devDependency!'))
+          } else if (!check.installed) {
+            result.missing.push(createWarning({content, offset, file, dep, check, error: true}, 'Dependency "' + dep.name + '" is missing from package.json and node_modules!'))
+          } else if (!check.isDep) {
+            result.missing.push(createWarning({content, offset, file, dep, check}, 'Dependency "' + dep.name + '" is missing from package.json! It is only indirectly installed and might be missing in production!'))
+          }
+        }
+      }
+    } catch (error) {
+      result.errors.push({file, error})
+    }
+  }
+
+  unused.forEach(u => {
+    result.unused.push({
+      dependency: u,
+      error: false,
+      message: 'Dependency "' + u + '" is never used! Move it to devDependencies or remove it!'
+    })
+  })
+
+  return result
+}
